70bf52d1150652285857b1e185660b10fd2a5c28
hector <hector@Hector.lan>
2015-06-24 21:12:36 -0700

    Adición de Ajax

--- src/java/com/requisiciones/entidades/controlador/UsuariosFacade.java
+++ src/java/com/requisiciones/entidades/controlador/UsuariosFacade.java
@@ -20,7 +20,7 @@ public class UsuariosFacade extends AbstractFacade<Usuarios> {
     private EntityManager em;
 
     @Override
-    protected EntityManager getEntityManager() {
+    public EntityManager getEntityManager() {
         return em;
     }
 
